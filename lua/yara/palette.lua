af91e435788e7a1225d8e145b6b853ff5be3ae1d
pompos02 <yiannis.caravellas@gmail.com>
2026-02-17 16:57:17 +0200

    style: better folds

--- lua/yara/palette.lua
+++ lua/yara/palette.lua
@@ -24,23 +24,6 @@ local palettes = {
         highlight_med = "#282828",
         highlight_high = "#52494e",
     },
-    -- tmmp = {
-    --     bg = "#ffffff",
-    --     bg_alt = "#eaeaea",
-    --     bg_highlight = "#acacac",
-    --     fg = "#16181a",
-    --     grey = "#7b8496",
-    --     blue = "#0057d1",
-    --     green = "#008b0c",
-    --     cyan = "#008c99",
-    --     red = "#d11500",
-    --     yellow = "#997b00",
-    --     magenta = "#d100bf",
-    --     pink = "#f40064",
-    --     orange = "#d17c00",
-    --     purple = "#a018ff",
-    --
-    -- }
     light = {
         bg = "#ffffff",
         nc = "#ffffff",
