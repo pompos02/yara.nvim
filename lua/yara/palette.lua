aae3e398882e860d13ded9c97e712475f8fbf52d
pompos02 <yiannis.caravellas@gmail.com>
2026-04-08 23:04:35 +0300

    feat: implementation of yara-low

--- lua/yara/palette.lua
+++ lua/yara/palette.lua
@@ -24,26 +24,40 @@ local palettes = {
 		highlight_med = "#282828",
 		highlight_high = "#52494e",
 	},
+
+	low = {
+		bg = "#181818",
+		nc = "#181818",
+		base = "#1a1a1a",
+		surface = "#222222",
+		overlay = "#2f2f2f",
+		muted = "#4d4d4d",
+		subtle = "#5b5a5a",
+		text = "#9d9da1",
+		red = "#b3575c",
+		gold = "#b5a76b",
+		rose = "#b3575c",
+		orange = "#756049",
+		blue = "#676f80",
+		greyblue = "#5e6b67",
+		lavender = "#656273",
+		purple = "#454954",
+		green = "#5c784d",
+
+		highlight_low = "#0c0c0c",
+		highlight_med = "#222222",
+		highlight_high = "#464045",
+	},
+
 	light = {
 		bg = "#ffffff",
 		nc = "#ffffff",
 		base = "#f5f5f5",
 		surface = "#f4f4ff",
 		overlay = "#e4e4e4",
-
 		muted = "#95a99f",
 		subtle = "#565f73",
-
 		text = "#000000",
-
-		red1 = "#d11500",
-		orange1 = "#634b2f",
-		gold1 = "#ff8c00",
-		rose1 = "#f40064",
-		blue1 = "#0057d1",
-		lavender1 = "#d100bf",
-		purple1 = "#c486f0",
-		green1 = "#008b0c",
 
 		red = "#d92f2f",
 		gold = "#de7a35",
@@ -52,7 +66,6 @@ local palettes = {
 		blue = "#0000ff",
 		greyblue = "#14a8bb",
 		lavender = "#a022c5",
-
 		purple = "#6c748a",
 		green = "#4fb324",
 
@@ -67,8 +80,12 @@ local function resolve_variant()
 		return "light"
 	end
 
+	if vim.g.colors_name == "yara-low" then
+		return "low"
+	end
+
 	local variant = options.variant or "auto"
-	if variant == "dark" or variant == "light" then
+	if variant == "dark" or variant == "light" or variant == "low" then
 		return variant
 	end
 
